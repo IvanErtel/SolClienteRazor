9c763ae38683ace1b68a0696eaf60446c240f324
Ivan Ertel <alanertel2275@gmail.com>
2023-10-09 13:29:11 -0300

    Agregar archivos de proyecto.

---
+++ ClienteRazor/Controllers/EnviosController.cs
@@ -0,0 +1,35 @@
+﻿using ClienteRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ClienteRazor.Controllers
+{
+    public class EnviosController : Controller
+    {
+        //Endpoints - extraemos url de SolServicioWebApi
+        string strURL = "https://localhost:44310/api/Shippers";
+        //Creamos instancia de la clase HttpClient
+        HttpClient cliente = new HttpClient();
+        public async Task<IActionResult> Index()
+        {
+            var listaShippers = JsonConvert.DeserializeObject<List<Shipper>>
+                (await cliente.GetStringAsync(strURL)).ToList();
+            return View(listaShippers);
+        }
+        [HttpGet]
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Agregar(Shipper shipper)
+        {
+            // llamamos al servicio de Api Rest
+            await cliente.PostAsJsonAsync(strURL, shipper);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ClienteRazor/Models/Shipper.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClienteRazor.Models
+{
+    public class Shipper
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int ShipperID { get; set; }
+        [Required]
+        [StringLength(40)]
+        [DisplayName("compañia de envio")]
+        public string? CompanyName { get; set; }
+        [StringLength(24)]
+        public string? Phone { get; set; }
+    }
+}
